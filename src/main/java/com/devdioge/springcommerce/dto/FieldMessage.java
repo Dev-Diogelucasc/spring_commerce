edb8f1ec4a01afd9232b11c48d5eb31c30861e16
Diogenes Cassimiro <dev.diogelucas@gmail.com>
2025-11-14 15:25:53 -0300

    Bean Validation

---
+++ src/main/java/com/devdioge/springcommerce/dto/FieldMessage.java
@@ -0,0 +1,12 @@
+package com.devdioge.springcommerce.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class FieldMessage {
+
+    private String fieldName;
+    private String message;
+}
--- src/main/java/com/devdioge/springcommerce/dto/ProductDTO.java
+++ src/main/java/com/devdioge/springcommerce/dto/ProductDTO.java
@@ -1,13 +1,22 @@
 package com.devdioge.springcommerce.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ProductDTO {
 
     private Long id;
+    // validação para nome, não vir vazio
+    @NotBlank(message = "Campo Requerido")
+    @Size(min = 3, max = 80, message = "O nome deve ter entre 3 e 80 caracteres")
     private String name;
+    @NotBlank(message = "Campo Requerido")
+    @Size(min = 10, message = "A descrição deve ter no minimo 10 caracteres")
     private String description;
+    @Positive(message = "O preço deve ser um valor positivo")
     private Double price;
     private String imgUrl;
 }
